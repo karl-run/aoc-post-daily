a59304edd7ffa3a53bf6f45ebecf49d73fcdbd0e
Karl J. Overå <k@rl.run>
2022-12-09 09:59:59 +0100

    fix stars order when not linear completion

--- post-daily.mjs
+++ post-daily.mjs
@@ -11,52 +11,54 @@ const result = await fetch(process.env.AOC_URL, {
 // Extract what we need from the data
 const mappedData = Object.values(result.members)
   .map((member) => {
-    const days = Object.values(member.completion_day_level).map((it) => ({
-      status: getDayStatus(it),
-    }));
-
-    const missingDays =
-      days.length < daysSinceFirst ? Array(daysSinceFirst - days.length).fill({ status: "Unfinished" }) : [];
+    const days = new Array(daysSinceFirst)
+      .fill({ status: "Unfinished" })
+      .map((it, index) => {
+        const day = member.completion_day_level[(index + 1).toString()];
+        return day != null ? { status: getDayStatus(day) } : it;
+      });
 
     return {
       name: member.name,
       score: member.local_score,
-      days: [...days, ...missingDays],
+      days: days,
     };
   })
   .sort((a, b) => b.score - a.score);
 
+const slackBody = {
+  blocks: [
+    {
+      type: "header",
+      text: {
+        type: "plain_text",
+        text: `:kermprehension: Advent of Code dag ${daysSinceFirst} :christmas_tree:`,
+        emoji: true,
+      },
+    },
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: createScoreboardMarkdown(mappedData),
+      },
+    },
+    {
+      type: "context",
+      elements: [
+        {
+          type: "mrkdwn",
+          text: `Postes daglig 22:00, kilde: https://github.com/karl-run/aoc-post-daily`,
+        },
+      ],
+    },
+  ],
+};
+
 // Post the scoreboard to Slack using markdown
 const slackResult = await fetch(process.env.SLACK_HOOK_FG_FUNKPROG, {
   method: "POST",
-  body: JSON.stringify({
-    blocks: [
-      {
-        type: "header",
-        text: {
-          type: "plain_text",
-          text: `:kermprehension: Advent of Code dag ${daysSinceFirst} :christmas_tree:`,
-          emoji: true,
-        },
-      },
-      {
-        type: "section",
-        text: {
-          type: "mrkdwn",
-          text: createScoreboardMarkdown(mappedData),
-        },
-      },
-      {
-        type: "context",
-        elements: [
-          {
-            type: "mrkdwn",
-            text: `Postes daglig 22:00, kilde: https://github.com/karl-run/aoc-post-daily`,
-          },
-        ],
-      },
-    ],
-  }),
+  body: JSON.stringify(slackBody),
 });
 
 // Need to process.exit(1) to make GHA fail if the script fails
@@ -94,6 +96,11 @@ function createStars(days) {
 
 function createScoreboardMarkdown(mappedData) {
   return mappedData
-    .map((it, index) => `${index + 1}) ${it.score.toString().padEnd(3, " ")}\t${createStars(it.days)} \t\t${it.name}`)
+    .map(
+      (it, index) =>
+        `${index + 1}) ${it.score.toString().padEnd(3, " ")}\t${createStars(
+          it.days
+        )} \t\t${it.name}`
+    )
     .join("\n");
 }
